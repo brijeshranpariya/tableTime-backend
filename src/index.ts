2375d7b91c8658cb40d4a1650da12549b18c68f2
brijesh <brijesh.ranpariya@prominentpixel.com>
2025-08-25 22:39:39 +0530

    removed the static port

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { pool } from "./db.js";
 import dotenv from "dotenv";
 import router from "./routes/router.js";
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server started");
   pool.query("SELECT NOW()", (err, res) => {
     if (err) {
