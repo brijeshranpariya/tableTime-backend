9a20fbe312825bdeb90628e2f8cd3a487cdf05f9
brijesh <brijesh.ranpariya@prominentpixel.com>
2025-08-25 22:30:44 +0530

    deployed the db

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,8 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
-  database: process.env.DB_NAME,
-  max: 10,
-  idleTimeoutMillis: 3000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
